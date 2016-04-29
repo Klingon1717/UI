adc438cdc5064a2d28f4619ffed0fe85e084c347
Whitney Murray <wnksmith92@yahoo.com>
2016-04-29 06:26:31 -0500

    used accordion function

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-$("#square").draggable();
-$("#green").draggable();
-$("#purple").resizable();
+$("#accordion").accordion();
+
+// $("#square").draggable();
+// $("#green").draggable();
+// $("#purple").resizable();
+// $("#target").droppable({
+// 	drop: function(ui, event){
+// 		$("#target").css("background-color", "red");
+// 	}
+// });
